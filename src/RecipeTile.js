b582914c42e019494dfbbb1291a9c285563c9587
Shishir Thapa <shishirthapa1@gmail.com>
2021-06-18 17:38:27 +0545

    change in recipe tile

--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './RecipeTile.css'
 
-export default function RecipeTile({recipe}) {
+export default function jjRecipeTile({recipe}) {
     return (
         recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
-        <div className="recipeTile">
+        <div className="recipeTile" onClick={() => {window.open(recipe["recipe"]["url"])}}>
             <img className="recipeTile__img" src={recipe["recipe"]["image"]} />
             <p className="recipeTile__name">{recipe["recipe"]["label"]}</p>
         </div>)
