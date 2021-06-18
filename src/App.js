c45922f82cdf2877d6e28faae42bd735c6ebde12
Shishir Thapa <shishirthapa1@gmail.com>
2021-06-18 17:26:10 +0545

    complete app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import {YOUR_APP_ID, YOUR_APP_KEY} from "./key";
+import Axios from "axios";
+import { useState } from "react";
+import RecipeTile from "./RecipeTile";
 
 function App() {
+
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [healthLabel, setHealthLabel] = useState('alcohol-free');
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabel}`
+
+  async function getRecipes() {
+    const result = await Axios.get(url);
+    setRecipes(result.data.hits);
+    console.log(result.data);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Food Recipe Plaza 🍔</h1>
+      <form className="app__searchForm" onSubmit={handleSubmit}>
+        
+        <input type="text" 
+        className="app__input"
+        placeholder="Enter ingredient" 
+        value={query} onChange={(e) => setQuery(e.target.value)}/>
+
+        <select className="app__healthLabels" onChange={(e) => setHealthLabel(e.target.value)}>
+          <option value='alcohol-free'>Alcohol Free</option>
+          <option value='vegan'>Vegan</option>
+          <option value='vegetarian'>Vegetarian</option>
+          <option value='low-sugar'>Low Sugar</option>
+        </select>
+
+        <input type="submit" className="app__submit" value="Search" />
+
+      </form>
+      
+      {/* <div className="app__recipes_count">
+        Showing {from} - {to}. Total found {count}
+      </div> */}
+
+      <div className="app__recipes">
+        {recipes.map((recipe, index) => {
+          return <RecipeTile recipe={recipe} key={index} />
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/key.js
@@ -0,0 +1,2 @@
+export const YOUR_APP_ID = "af9fd85b";
+export const YOUR_APP_KEY = "3d98562f7478d9135688f39c1c05bfaa";
